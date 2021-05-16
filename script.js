80390d0d9f33b9757ca6ded378a6df92f2d1fa29
un7nown <pathen19@gmail.com>
2021-05-16 18:12:58 +0200

    letter tiles added
    Addded letter tiles, changed script position in html.

--- script.js
+++ script.js
@@ -6,4 +6,25 @@ const game = {
     elemAttempts: document.querySelector(".attempts"), //element z liczba prob
     elemLetters: document.querySelector(".letter-board"), //lista z literkami do klikania
     keywords: ["Money", "Rectangle", "Flower", "Cucumber", "Horse", "Carpet"],
-}
+
+    generateLetters() {
+        const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split("");
+        console.log(alphabet);
+
+        alphabet.forEach(letter => {
+            const button = document.createElement("button");
+            button.classList.add("letter-tile");
+            button.type = "button";
+            button.dataset.letter = letter;
+            button.innerText = letter;
+            this.elemLetters.appendChild(button);
+        });
+    },
+
+    initBoard() {
+        this.generateLetters();
+    }
+};
+
+const btnStart = document.querySelector(".start");
+btnStart.addEventListener("click", game.initBoard())
